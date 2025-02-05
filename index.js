6f4d410b8e6d6b08896102f3d30eb8413efd09df
GabrielAntiquera <gabriel.antiqueras@gmail.com>
2025-02-06 02:24:58 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,29 +1,57 @@
-const hiddenElements = document.querySelectorAll('.hidden');
+let Main = document.getElementById("robo");
 
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        }
-    });
-});
-hiddenElements.forEach((el) => observer.observe(el));
+const Sandbox = () => {
+  Main.src = "Content/Sandbox.webp";
+}
 
-// Code for the toggling of the website Nav Menu
-const toggleNav = () => {
-    document.body.dataset.nav = document.body.dataset.nav === "true" ? "false" : "true";
+const Bloom = () => {
+  Main.src = "Content/Bloom_Icon.webp";
+}
+
+const Adrik = () => {
+  Main.src = "Content/Evening With Adrik.webp";
+}
+
+const Petro = () => {
+  Main.src = "Content/Petroglyph.jpg";
+}
+
+const Chow = () => {
+  Main.src = "Content/SG CHOW.webp";
+}
+
+const School = () => {
+  Main.src = "Content/Nile Front.webp";
+}
+
+const Personal = () => {
+  Main.src = "Content/Summer.webp";
+}
+
+// Robo animation//
+let paths = ["Content/Robo2.png","Content/Robo1.png","Content/Robo1.png","Content/Robo1.png"]
+  
+  let img = document.getElementById("robo");
+  let i = 0;
+  let timer = setInterval(function(){
+  // If we've reached the end of the array...
+  if(i >= paths.length){
+  i=0;
+  }
+  img.src = paths[i++]; // Sete the path to the current counter and then increase the counter
+  }, 400);
+
+  const StopRobo =  () => {
+    clearInterval(timer);
   }
 
 //Image Popup
-document.querySelectorAll('.Works_Pages img').forEach(image => {
+document.querySelectorAll('img').forEach(image => {
   image.onclick = () => {
   document.querySelector('.imagePopUp').style.display = 'block';
   document.querySelector('.imagePopUp img').src = image.getAttribute("src");
   document.querySelector('.imagePopUp').classList.add('popupEnter');
   document.querySelector('html').style.overflow = 'hidden';
-  document.querySelector('#nav-toggle').style.display = 'none';
-  document.querySelector('.backButton').style.display = 'none';
   }
 });
 
@@ -31,14 +59,10 @@ document.querySelector('.imagePopUp span').onclick = () => {
   document.querySelector('.imagePopUp').style.display = 'none';
   document.querySelector('.imagePopUp').classList.remove('popupEnter');
   document.querySelector('html').style.overflow = 'auto';
-  document.querySelector('#nav-toggle').style.display = 'block';
-  document.querySelector('.backButton').style.display = 'block';
 }
 
 document.querySelector('.imagePopUp .overlay').onclick = () => {
   document.querySelector('.imagePopUp').style.display = 'none';
   document.querySelector('.imagePopUp').classList.remove('popupEnter');
   document.querySelector('html').style.overflow = 'auto';
-  document.querySelector('#nav-toggle').style.display = 'block';
-  document.querySelector('.backButton').style.display = 'block';
 }
